7c94b3c3933c8de12ea3d795daafd06c21e04d7d
Andreas Reng Mogensen <andreas@somethingworked.com>
2018-04-17 13:08:28 +0200

    Initial GUI with limited functionality. Can retrieve frames from video and place them into directory and read from said directory if frames were already extracted

--- tv-subtitle-sorting/src/main/java/SelectVideo.java
+++ tv-subtitle-sorting/src/main/java/SelectVideo.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 
 public class SelectVideo extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
-    private JButton buttonCancel;
+    private JTextField videoTextField;
+    private JButton selectVideoButton;
+    private JButton selectWorkingButton;
+    private JTextField workingTextField;
+    private JCheckBox cleanExistingFilesCheckBox;
+    private File videoFile;
+    private File workingDirectory;
 
     public SelectVideo() {
         setContentPane(contentPane);
@@ -13,28 +21,62 @@ public class SelectVideo extends JDialog {
 
         buttonOK.addActionListener(e -> onOK());
 
-        buttonCancel.addActionListener(e -> onCancel());
+//        buttonCancel.addActionListener(e -> onCancel());
+
+        selectVideoButton.addActionListener(e -> onVideoSelect());
+        selectWorkingButton.addActionListener(e -> onWorkSelect());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                onCancel();
+                dispose();
             }
         });
 
+        this.pack();
+        this.setVisible(true);
+
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        //contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onVideoSelect(){
+        JFileChooser jFileChooser = new JFileChooser();
+        int response = jFileChooser.showOpenDialog(SelectVideo.this);
+
+        if(response == JFileChooser.APPROVE_OPTION){
+            videoTextField.setText(jFileChooser.getSelectedFile().toString());
+            videoFile = jFileChooser.getSelectedFile();
+        } else {
+            dispose();
+        }
+
+    }
+
+    private void onWorkSelect(){
+        JFileChooser jFileChooser = new JFileChooser();
+        int response = jFileChooser.showOpenDialog(SelectVideo.this);
+
+        if(response == JFileChooser.APPROVE_OPTION){
+            workingTextField.setText(jFileChooser.getSelectedFile().toString());
+            workingDirectory = jFileChooser.getSelectedFile();
+        } else {
+            dispose();
+        }
+
     }
 
     private void onOK() {
-        // add your code here
         dispose();
     }
 
-    private void onCancel() {
-        // add your code here if necessary
-        dispose();
+    public File getVideoFile() {
+        return videoFile;
+    }
+
+    public File getWorkingDirectory() {
+        return workingDirectory;
     }
 
     public static void main(String[] args) {
--- tv-subtitle-sorting/src/main/java/Sorter.java
+++ tv-subtitle-sorting/src/main/java/Sorter.java
@@ -1,21 +1,130 @@
+import dk.kb.tvsubtitleocr.lib.frameextraction.FrameExtractionProcessor;
+import dk.kb.tvsubtitleocr.lib.frameextraction.IFrameExtractionProcessor;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 public class Sorter {
 
+    // Local fields
+    private Path videoFile;
+    private Path workingDirectory;
+    private File[] frameFiles;
+    private Path framesPath;
+
+
+    // Swing components
     private JPanel topPanel;
     private JButton positiveButton;
     private JButton negativeButton;
     private JButton sameButton;
     private JLabel imageLabel;
+    private SelectVideo selectVideo;
 
     public Sorter() {
+        EventQueue.invokeLater(this::run);
     }
 
-    public static void main(String[] args) {
+    private void createAndShow() {
         JFrame frame = new JFrame("Sorter");
-        frame.setContentPane(new Sorter().topPanel);
+
+        negativeButton.addActionListener(e -> nextFrame());
+
+        sameButton.addActionListener(e -> nextFrame());
+
+        positiveButton.addActionListener(e -> nextFrame());
+
+        frame.setContentPane(this.topPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
+
+    private void onNegative() {
+
+        nextFrame();
+
+    }
+
+    private void onSame() {
+
+        nextFrame();
+    }
+
+    private void onPositive() {
+
+        nextFrame();
+    }
+
+    private void nextFrame() {
+        ImageIcon icon = null;
+        try {
+            icon = new ImageIcon(ImageIO.read(frameFiles[0]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        imageLabel.setText("");
+        imageLabel.setIcon(icon);
+    }
+
+    private void run() {
+        createAndShow();
+        Dialog dlg = new SelectVideo();
+
+        try {
+            workingDirectory = ((SelectVideo) dlg).getWorkingDirectory().toPath();
+        } catch (NullPointerException e) {
+            workingDirectory = Paths.get(System.getProperty("user.dir"));
+            try {
+                workingDirectory = Paths.get(workingDirectory.toFile().getAbsolutePath(), "working");
+
+                if (!Files.exists(workingDirectory))
+                    Files.createDirectory(workingDirectory);
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } finally {
+            framesPath = Paths.get(workingDirectory.toFile().getAbsolutePath(), "frames");
+        }
+
+        try {
+            videoFile = ((SelectVideo) dlg).getVideoFile().toPath();
+
+
+            Executors.newSingleThreadExecutor().submit(() -> {
+                try {
+                    if (!Files.exists(framesPath)) {
+                        Files.createDirectory(framesPath);
+                    }
+
+                    IFrameExtractionProcessor frameExtractor = new FrameExtractionProcessor(workingDirectory.toFile());
+                    ((FrameExtractionProcessor) frameExtractor).extractVideoFramesToDisk(videoFile.toFile(), 1, framesPath.toFile());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).get();
+
+        } catch (NullPointerException ignored) {
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        finally {
+            File[] listOfFiles = framesPath.toFile().listFiles(f -> f.getName().endsWith(".png"));
+            Arrays.sort(listOfFiles);
+            frameFiles = listOfFiles;
+        }
+
+    }
+
 }
+
